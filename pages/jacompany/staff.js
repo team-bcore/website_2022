0c001ceed293fd22bef72dc60ef7eff4fcfe1cc8
Satomi <s-tanaka@bcore.co.jp>
2022-06-08 16:56:03 +0900

    fix staff.js

--- pages/jacompany/staff.js
+++ pages/jacompany/staff.js
@@ -1,6 +1,6 @@
 import Navbar from "../../components/Navbar";
-import Introduction from "../../components/Company/Introduction";
-import Navstaff from "../../components/Company/Navstaff";
+import Introduction from "../../components/company/Introduction";
+import Navstaff from "../../components/company/Navstaff";
 import Footer from "../../components/Footer";
 
 export default function Jacompany() {
