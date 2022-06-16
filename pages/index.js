51739567ad7579845b274daed0abff34894ab011
Satomi <s-tanaka@bcore.co.jp>
2022-06-16 19:25:54 +0900

    fix card 3

--- pages/index.js
+++ pages/index.js
@@ -36,14 +36,14 @@ export default function Home({ allNewsData }) {
             "私たちは、独自に開発した自動認識コード「カラービット®︎」やエッジAIカメラを用いて、現場でのニッチなお悩みを解決し、業務の効率化を促進するソリューションを提供しています。",
           images: [
             {
-              url: "../images/SEO/top-card.png",
+              url: "public/images/SEO/top-card.png",
               width: 800,
               height: 600,
               alt: "ビーコア株式会社イメージ",
               type: "image/png",
             },
             {
-              url: "../images/SEO/top-card.png",
+              url: "public/images/SEO/top-card.png",
               width: 900,
               height: 800,
               alt: "ビーコア株式会社イメージ",
