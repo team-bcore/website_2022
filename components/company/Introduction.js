a5685c834b06d1b891fde61e92c59049936c86d4
Satomi <s-tanaka@bcore.co.jp>
2022-06-16 17:02:45 +0900

    add explanation

--- components/company/Introduction.js
+++ components/company/Introduction.js
@@ -6,7 +6,7 @@ export default function Introduction() {
           スタッフ紹介
         </h2>
         <p className="font-noto text-gray-900 text-2xl sm:px-4">
-          水野廉郎　Yasuro Mizuno
+          水野 廉郎　Yasuro Mizuno
         </p>
         <p className="pt-4 font-noto text-base text-gray-900 md:text-lg sm:px-4">
           肩書
@@ -23,10 +23,12 @@ export default function Introduction() {
           マサチューセッツ工科大学研究員。ゼネコンにて設計・研究開発・海外業務・経営企画業務などに関わる。
           2003年に環境コンサルティング会社（土壌汚染対策・残土リサイクル）を起業後、
           2009年よりビーコア株式会社の経営に参画。2011年より代表取締役社長に就任。
+          <br></br>
+          趣味は離島旅行と水中写真撮影（マクロ）。アート全般。CDとレコード収集。酒器集め。自転車。エンタメが大好きで、伝統芸能（歌舞伎・文楽・能狂言）・コンサート（クラシックとジャズ）・落語会・寄席のどれかにできれば週2回の頻度で行けるよう、ワークライフバランスの実現に向けて奮闘中。
         </p>
         <hr className="w-full my-8 border-gray-300" />
         <p className="font-noto text-gray-900 text-2xl sm:px-4">
-          杉山典男　Norio Sugiyama
+          杉山 典男　Norio Sugiyama
         </p>
         <p className="pt-4 font-noto text-base text-gray-900 md:text-lg sm:px-4">
           肩書
@@ -48,13 +50,13 @@ export default function Introduction() {
         </p>
         <hr className="w-full my-8 border-gray-300" />
         <p className="font-noto text-gray-900 text-2xl sm:px-4">
-          田中　葉　Yo Tanaka
+          田中 葉　Yo Tanaka
         </p>
         <p className="pt-4 font-noto text-base text-gray-900 md:text-lg sm:px-4">
           肩書
         </p>
         <p className="mt-3 font-ud text-base text-gray-700 md:text-lg sm:px-4">
-          技術開発者
+          マスターエンジニア
         </p>
         <p className="pt-4 font-noto text-base text-gray-900 md:text-lg sm:px-4">
           主な職域
@@ -66,7 +68,9 @@ export default function Introduction() {
           プロフィール
         </p>
         <p className="mt-3 font-ud text-base text-gray-700 md:text-lg sm:px-4">
-          大学院終了後、5年間開発者としてバーコードリーダーを製造開発している会社に勤務し、バーコードリーダーの光学・機構・生産治具などの設計のほかに波形処理、プログラミングなど多岐にわたる業務をこなす。その後ビーコアへ入社し、カラービット技術の根幹であるデコードアルゴリズムの新規開発を行う。
+          大学院終了後、5年間開発者としてバーコードリーダーを製造開発している会社に勤務し、バーコードリーダーの光学・機構・生産治具などの設計のほかに波形処理、プログラミングなど多岐にわたる業務をこなす。
+          <br></br>
+          その後ビーコアへ入社し、カラービット技術の根幹であるデコードアルゴリズムの新規開発を行う。
           やる気が出れば仕事が早いマイペースな開発者。ビーコアのスタッフたちに、やる気スイッチを押してもらいながら楽しく開発に従事している。「仕事でもなんでも楽しく」がモットー。
           <br></br>
           最近は自転車とメトロイドヴァニアというジャンルのゲームにハマっている。自転車は飯島さんの自転車通勤がきっかけで始めたが、実際に始めてみたら、夏でも冬でも走るのが楽しい。とにかく楽しい！電車通勤サヨナラです。
@@ -74,13 +78,13 @@ export default function Introduction() {
         </p>
         <hr className="w-full my-8 border-gray-300" />
         <p className="font-noto text-gray-900 text-2xl sm:px-4">
-          飯島　圭一朗　Keiichiro Iijima
+          飯島 圭一朗　Keiichiro Iijima
         </p>
         <p className="pt-4 font-noto text-base text-gray-900 md:text-lg sm:px-4">
           肩書
         </p>
         <p className="mt-3 font-ud text-base text-gray-700 md:text-lg sm:px-4">
-          技術マネージャー
+          マネージャー兼チーフエンジニア
         </p>
         <p className="pt-4 font-noto text-base text-gray-900 md:text-lg sm:px-4">
           主な職域
@@ -92,9 +96,10 @@ export default function Introduction() {
           プロフィール
         </p>
         <p className="mt-3 font-ud text-base text-gray-700 md:text-lg sm:px-4">
-          大学卒業後、SESとしてメガバンクのインフラエンジニアとして3年間従事したのち大手通信事業会社に転職。転職後は自社の監視運用基盤の保守運用を行いつつ社内外のシステム監視運用責任者として7年間従事。
-          出張以外は関東圏から出られずディズニーランドに入園後すぐに呼び出されるといった壮絶な生活を送る。
-          2018年7月にビーコアへ入社。VRKを含むインフラの保守運用を行いながらお客様への企画提案を担当。
+          大学卒業後、SESとしてメガバンクのインフラエンジニアとして3年間従事したのち大手通信事業会社に転職。転職後は自社の監視運用基盤の保守運用を行いつつ、社内外のシステム監視運用責任者として7年間従事。
+          出張以外は関東圏から出られず、ディズニーランドに入園後すぐに呼び出されるといった壮絶な生活を送る。
+          <br></br>
+          2018年7月にビーコアへ入社。Vieurekaカメラを含むインフラの保守運用を行いながら、お客様への企画提案を担当。
           先端技術者が集まるビーコアでは「潤滑油」のような役割を担っている。
           <br></br>
           健康のために日々自転車通勤を行い、企画提案に必要な知識を増やすべく月に2冊以上の本を読む。会話術も大事と考え、妻と一緒に落語や講談を嗜んでいる。
@@ -103,7 +108,7 @@ export default function Introduction() {
         </p>
         <hr className="w-full my-8 border-gray-300" />
         <p className="font-noto text-gray-900 text-2xl sm:px-4">
-          渡邉友浩　Tomohiro Watanabe
+          渡邉 友浩　Tomohiro Watanabe
         </p>
         <p className="pt-4 font-noto text-base text-gray-900 md:text-lg sm:px-4">
           肩書
@@ -112,10 +117,18 @@ export default function Introduction() {
           エンジニア
         </p>
         <p className="pt-4 font-noto text-base text-gray-900 md:text-lg sm:px-4">
+          主な職域
+        </p>
+        <p className="mt-3 font-ud text-base text-gray-700 md:text-lg sm:px-4">
+          3D設置シミュレーション制作・縮尺ミニチュア部品等の製作
+        </p>
+        <p className="pt-4 font-noto text-base text-gray-900 md:text-lg sm:px-4">
           プロフィール
         </p>
         <p className="mt-3 font-ud text-base text-gray-700 md:text-lg sm:px-4">
-          工業高校電気科卒業後、ITVカメラメーカーに7年間従事。サービスエンジニアとしてシステム設計、製造業務に携わる。2020年ビーコア入社。主に3Dプリンターを使って、お客様がVRKをより快適に使っていただけるような治具を開発したり、実証実験に使う部品を製作。エンジニアとしての経験は前職7年、現職2年。趣味はランニング、DIY。最近はまっていることは、自宅の3Dモデリング。
+          工業高校電気科卒業後、ITVカメラメーカーに7年間従事。サービスエンジニアとしてシステム設計、製造から現地調整までワンオペでこなす。依頼があればすぐ現地に駆け付けるスタイルで年中全国各地を回り、スキマ時間でBarめぐりを楽しむ。
+          <br></br>体がボロボロになったあたりで2020年ビーコア入社。現職で3Dプリンターに出会い、モデリングにドハマりする。業務では3Dでの設置シミュレーションや実証実験に使う縮尺ミニチュア部品等を製作。
+          <br></br>マイティソーになるべく肉体改造中。定年後は福岡に移住して余生を過ごすのが夢。
         </p>
         <hr className="w-full my-8 border-gray-300" />
       </div>
