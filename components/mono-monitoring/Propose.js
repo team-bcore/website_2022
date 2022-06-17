ff46a4aab789f89496cf0b10ae94673e376c103d
Satomi <s-tanaka@bcore.co.jp>
2022-06-17 18:14:17 +0900

    Fix class

--- components/mono-monitoring/Propose.js
+++ components/mono-monitoring/Propose.js
@@ -29,11 +29,11 @@ export default function Purpose() {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M5 13l4 4L19 7"
                   />
                 </svg>
@@ -56,11 +56,11 @@ export default function Purpose() {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M5 13l4 4L19 7"
                   />
                 </svg>
@@ -83,11 +83,11 @@ export default function Purpose() {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M5 13l4 4L19 7"
                   />
                 </svg>
