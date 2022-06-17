8c86d939fff79097559a7d46d34ead3d9a7ba311
Satomi <s-tanaka@bcore.co.jp>
2022-06-17 15:00:19 +0900

    SEO

--- pages/jacompany/staff.js
+++ pages/jacompany/staff.js
@@ -2,10 +2,50 @@ import Navbar from "../../components/Navbar";
 import Introduction from "../../components/company/Introduction";
 import Navstaff from "../../components/company/Navstaff";
 import Footer from "../../components/Footer";
+import { NextSeo } from "next-seo";
+import Head from "next/head";
+
 
 export default function Jacompany() {
   return (
     <div className="">
+      <>
+       <NextSeo
+        title="ビーコア株式会社"
+        description="現場のニッチなお悩みをIoTの力で解決します"
+        openGraph={{
+          url: "https://www.bcore.biz/jacompany/staff",
+          title: "スタッフ紹介",
+          description:
+            "さまざまな分野のエキスパートがお客様の課題解決にあたる少数精鋭チームです。",
+          images: [
+            {
+              url: "https://friendly-macaron-19aef4.netlify.app/images/SEO/staff-card.png",
+              width: 800,
+              height: 600,
+              alt: "スタッフ紹介イメージ",
+              type: "image/png",
+            },
+            {
+              url: "https://friendly-macaron-19aef4.netlify.app/images/SEO/staff-card.png",
+              width: 900,
+              height: 800,
+              alt: "スタッフ紹介イメージ",
+              type: "large_image/png",
+            },
+          ],
+          site_name: "",
+        }}
+        twitter={{
+          handle: "colorbit_bcore",
+          site: "https://twitter.com/colorbit_bcore",
+          cardType: "summary_large_image",
+        }}
+      />
+      </>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Introduction />
       <Navstaff />
