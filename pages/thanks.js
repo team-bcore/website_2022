26ae0a05dbb912471108217146c5c56829297964
Satomi <s-tanaka@bcore.co.jp>
2022-06-17 14:59:38 +0900

    Fix text

--- pages/thanks.js
+++ pages/thanks.js
@@ -1,16 +1,26 @@
 import Link from "next/link";
+import Image from "next/image";
+
 
 export default function Thank() {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-screen-sm sm:text-center sm:mx-auto">
-        <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-primary"></div>
-
+        <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-white">
+          <div className="pt-1 flex flex-shrink-0">
+            <Image
+              src="/images/Footer/logo.svg" // Route of the image file
+              height={31.7} // Desired size with correct aspect ratio
+              width={100} // Desired size with correct aspect ratio
+              alt="bcore_logo"
+            />
+          </div>
+        </div>
         <h2 className="mb-4 font-noto text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-          Thank you!
+          お問い合わせありがとうございます
         </h2>
         <p className="font-ud text-base text-gray-700 md:text-lg sm:px-4">
-          メッセージは送信されました。折り返しメールにてご連絡差し上げます。
+          お問い合わせいただきありがとうございます。<br></br>2～3営業日以内に、担当よりメールにて返信させていただきます。
         </p>
         <hr className="w-full my-8 border-gray-300" />
         <Link href="/">
