2d2512574edb4954a41f9a867df342de87892896
Satomi <s-tanaka@bcore.co.jp>
2022-06-19 22:00:11 +0900

    Add google analytics tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import "../styles/global.css";
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
+  <div className="container">
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+          ga('create', 'G-EP8V6PRVC6', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
+      <Script
+        src="https://www.google-analytics.com/analytics.js"
+        strategy="afterInteractive"
+      />
+    </div>
   return <Component {...pageProps} />;
+
+  
 }
