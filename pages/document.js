a3061b031c846efb6fbbfa54b3a24171c99a8579
Satomi <s-tanaka@bcore.co.jp>
2022-06-20 16:37:51 +0900

    put no index

--- pages/document.js
+++ pages/document.js
@@ -1,10 +1,19 @@
 import Navbar from "../components/Navbar";
 import Doc from "../components/Doc";
 import Footer from "../components/Footer";
+import { NextSeo } from "next-seo";
+import Head from "next/head";
 
 export default function Document() {
   return (
     <div className="">
+      <>
+        <NextSeo nofollow={true} />
+        <p>This page is not followed</p>
+      </>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Doc />
       <Footer />
