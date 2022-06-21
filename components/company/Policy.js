bf72f7360afdb045df1ab9505e97832b835e29fc
Satomi <s-tanaka@bcore.co.jp>
2022-06-21 10:34:54 +0900

    fix spell mistake

--- components/company/Policy.js
+++ components/company/Policy.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Policy() {
   return (
-    <div className="bg-Cmpany-bg bg-cover bg-no-repeat">
+    <div className="bg-Company-bg bg-cover bg-no-repeat">
       <div className="px-4 pt-32 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-40">
         <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
           <div className="flex flex-col sm:text-center sm:mb-0">
