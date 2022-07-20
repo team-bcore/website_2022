bd07bd3d6dd51282c5ee2bc948e790276c8ca981
Satomi <s-tanaka@bcore.co.jp>
2022-07-20 18:45:59 +0900

    add seo

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -9,44 +9,45 @@ import Cta from "../../components/Cta";
 import BlogSeo from "../../components/BlogSeo";
 
 export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
-    return {
-        props: {
-            postData,
-        },
-    };
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
 export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-        paths,
-        fallback: false,
-    };
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export default function Post({ postData }) {
-    const { code, frontmatter } = postData;
-    const Component = useMemo(() => getMDXComponent(code), [code]);
-    return (
-      <Layout>
-        <div className="px-2 py-4 mx-auto sm:max-w-xl md:max-w-3xl lg:max-w-screen-3xl lg:py-8">
-          <div className="max-w-screen-sm sm:text-center sm:mx-auto">
-            <div className="prose lg:prose-lg">
-              <h2 className="font-noto text-left">{frontmatter.title}</h2>
-              <p className="font-ud text-left">{frontmatter.description}</p>
-              <p className="font-ud text-right">{frontmatter.date}</p>
-              <article className="font-ud text-left">
-                <Component
-                  components={{
-                    Cta,
-                    BlogSeo,
-                  }}
-                />
-              </article>
-            </div>
+  const { code, frontmatter } = postData;
+  const Component = useMemo(() => getMDXComponent(code), [code]);
+  return (
+    <Layout>
+      <BlogSeo frontmatter={frontmatter}/>
+      <div className="px-2 py-4 mx-auto sm:max-w-xl md:max-w-3xl lg:max-w-screen-3xl lg:py-8">
+        <div className="max-w-screen-sm sm:text-center sm:mx-auto">
+          <div className="prose lg:prose-lg">
+            <h2 className="font-noto text-left">{frontmatter.title}</h2>
+            <p className="font-ud text-left">{frontmatter.description}</p>
+            <p className="font-ud text-right">{frontmatter.date}</p>
+            <article className="font-ud text-left">
+              <Component
+                components={{
+                  Cta,
+                  BlogSeo,
+                }}
+              />
+            </article>
           </div>
         </div>
-      </Layout>
-    );
-  }
+      </div>
+    </Layout>
+  );
+}
