2cc00480dc853b89e915c8457f3f5c13271adca4
Satomi <s-tanaka@bcore.co.jp>
2022-07-21 11:15:39 +0900

    remove Head

--- components/bloglayout.js
+++ components/bloglayout.js
@@ -13,21 +13,6 @@ export default function Layout({ children, home }) {
     <div className="bg-gray-100">
       <div className="pt-8">
         <div className={styles.container}>
-          <Head>
-            <link rel="icon" href="/favicon.ico" />
-            <meta
-              name="description"
-              content="テクノロジー、デザイン、ビジネスなどさまざまなトピックについて書いています。"
-            />
-            <meta
-              property="og:image"
-              content={`https://og-image.vercel.app/${encodeURI(
-                siteTitle
-              )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-            />
-            <meta name="og:title" content={siteTitle} />
-            <meta name="twitter:card" content="summary_large_image" />
-          </Head>
           <header className={styles.header}>
             {home ? (
               <>
