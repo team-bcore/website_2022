f39ef4d532fc56011b8af0f11eb29ed5cd72411a
Satomi <s-tanaka@bcore.co.jp>
2022-07-29 14:12:19 +0900

    change bg-color

--- components/News.js
+++ components/News.js
@@ -7,7 +7,7 @@ import Date from "./date";
 
 export default function News({ allNewsData }) {
   return (
-    <div className="py-16 bg-gray-200">
+    <div className="py-16 bg-blue-100">
       <div className="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-4">
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <div className="grid grid-cols-12">
