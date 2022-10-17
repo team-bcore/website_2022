bd8faf0b743e43e43030f79b0e30742a331a72bd
Satomi <s-tanaka@bcore.co.jp>
2022-10-17 21:10:46 +0900

    add new page

---
+++ pages/job/solutions.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Herosolutions from "../../components/job-content/Herosolutions";
+
+import Footer from "../../components/Footer";
+
+export default function Solutions() {
+    return (
+        <div className="">
+        <Herosolutions/>
+       
+        <Footer />
+        </div>
+    );
+}
