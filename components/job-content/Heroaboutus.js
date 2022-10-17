ed9ba91defe81c2d3a0faf884926d3e999cf6c8e
Satomi <s-tanaka@bcore.co.jp>
2022-10-17 21:08:34 +0900

    add div

--- components/job-content/Heroaboutus.js
+++ components/job-content/Heroaboutus.js
@@ -1,5 +1,6 @@
 export default function Heroaboutus() {
     return (
+      <div className="">
       <div className="bg-Job-aboutus-hero bg-cover bg-no-repeat">
         <div className="px-4 pt-32 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-40">
           <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
@@ -12,6 +13,7 @@ export default function Heroaboutus() {
             </div>
           </div>
         </div>
+      </div>
       </div>
     );
   }
