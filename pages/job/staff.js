3258ce7de803069bd47208d793dbadec5cd8d8fc
Satomi <s-tanaka@bcore.co.jp>
2022-10-25 13:45:37 +0900

    add component

--- pages/job/staff.js
+++ pages/job/staff.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Navbar from "../../components/job-content/Navbar";
 import Herostaff from "../../components/job-content/Herostaff";
-
+import Contentstaff from "../../components/job-content/Contentstaff";
 import Footer from "../../components/Footer";
 
 export default function Solutions() {
@@ -9,6 +9,7 @@ export default function Solutions() {
         <div className="">
             <Navbar />
             <Herostaff/>
+            <Contentstaff/>
             <Footer />
         </div>
     );
