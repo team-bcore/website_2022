b095a11ef67a9584d692922a8e4169a5e24ad837
Satomi <s-tanaka@bcore.co.jp>
2022-10-25 13:48:22 +0900

    midified component

--- components/job-content/Message.js
+++ components/job-content/Message.js
@@ -4,37 +4,29 @@ import Image from "next/image";
 export default function Message() {
     return (
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-16">
-            <div className="max-w-screen-sm sm:text-center sm:mx-auto">
-                <h2 className="mb-8 text-3xl font-noto tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                    代表からのメッセージ
-                </h2>
-                <hr className="w-full my-8 border-gray-300" />
-            </div>
             <div className="py-16 grid gap-10 lg:grid-cols-2">
-                <div className="lg:pr-10">
-                    <h5 className="mb-4 text-4xl font-extrabold leading-none">
-                        メッセージタイトル
+                <div className="pt-16 lg:pr-10">
+                    <div className="">
+                        <p className="inline-block px-3 py-px mb-4 text-base font-ud tracking-wider text-teal-900 uppercase rounded-full bg-amber-300">
+                            代表からのメッセージ
+                        </p>
+                    </div>
+                    <p className="pt-8 mb-3 font-ud text-gray-800 text-lg">ビーコア株式会社 代表取締役社長</p>
+                    <h5 className="text-gray-800 mb-3 text-2xl font-noto leading-none sm:text-3xl">
+                        水野廉郎　Yasuro Mizuno
                     </h5>
-                    <p className="pt-6 font-ud text-lg text-gray-900 md:text-xl">
-                        ビーコア株式会社 代表取締役
-                        <br></br>水野廉郎
+                    <hr className="mb-4 border-gray-300" />
+                    <p className="text-gray-700 font-ud">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
-                    <p className="pt-6 mb-6 text-gray-900">
-
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                        quae explicabo. Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem totam rem aperiam, eaque ipsa quae explicabo.
-                    </p>
-                    <hr className="mb-5 border-gray-300" />
                 </div>
-                <div>
+                <div className="pt-8 drop-shadow-lg">
                     <Image
-                        className=""
-                        src="/images/Job/mizuno-image-white.png" // Route of the image file
-                        height={800} // Desired size with correct aspect ratio
-                        width={1320} // Desired size with correct aspect ratio
-                        alt="CEO-pic"
+                        className="rounded shadow-lg sm:h-96 xl:h-24"
+                        src="/images/Job/ceo.jpg" // Route of the image file
+                        height={2651} // Desired size with correct aspect ratio
+                        width={3535} // Desired size with correct aspect ratio
+                        alt="CEO"
                     />
                 </div>
             </div>
