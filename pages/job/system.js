a14c13112e22522c21cc56f0e898f4b0c13f85ae
Satomi <s-tanaka@bcore.co.jp>
2022-10-26 10:41:30 +0900

    add component

--- pages/job/system.js
+++ pages/job/system.js
@@ -1,16 +1,17 @@
 import Head from "next/head";
-import Navbar from "../../components/job-content/Navbar";
+import Navbar2 from "../../components/job-content/Navbar2";
 import Herosystem from "../../components/job-content/Herosystem";
 import Contentsystem from "../../components/job-content/Contentsystem";
-
+import Featuressystem from "../../components/job-content/Featuressystem";
 import Footer from "../../components/Footer";
 
 export default function System() {
     return (
         <div className="">
-            <Navbar />
+            <Navbar2 />
             <Herosystem />
             <Contentsystem />
+            <Featuressystem />
             <Footer />
         </div>
     );
