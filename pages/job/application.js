84b711f8ee1afebe557e88d89266aa630e12fa4c
Satomi <s-tanaka@bcore.co.jp>
2022-10-31 16:53:30 +0900

    add seo

--- pages/job/application.js
+++ pages/job/application.js
@@ -4,10 +4,47 @@ import Heroapplication from "../../components/job-content/Heroapplication";
 import Contentapplication from "../../components/job-content/Contentapplication";
 import Featuresapplication from "../../components/job-content/Featuresapplication";
 import Footer from "../../components/Footer";
+import { NextSeo } from "next-seo";
 
-export default function Aboutus() {
+export default function Application() {
     return (
         <div className="">
+            <>
+        <NextSeo
+          title="ビーコア株式会社"
+          description="現場のニッチなお悩みをIoTの力で解決します"
+          openGraph={{
+            url: "https://www.bcore.biz/job/application",
+            title: "募集要項",
+            description: "現在募集中の職種について知ることができます。",
+            images: [
+              {
+                url: "https://bcore.biz/images/SEO/job-seo-image.png",
+                width: 800,
+                height: 420,
+                alt: "採用情報イメージ",
+                type: "image/png",
+              },
+            //   {
+            //     url: "https://bcore.biz/images/SEO/mission-card.png",
+            //     width: 900,
+            //     height: 800,
+            //     alt: "ポリシーイメージ",
+            //     type: "large_image/png",
+            //   },
+            ],
+            site_name: "",
+          }}
+          twitter={{
+            handle: "colorbit_bcore",
+            site: "https://twitter.com/colorbit_bcore",
+            cardType: "summary_large_image",
+          }}
+        />
+      </>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
             <Navbar2 />
             <Heroapplication />
             <Contentapplication />
