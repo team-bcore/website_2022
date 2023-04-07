fa4d43a5a7ffb8762f8c3285ba35e670de406bb1
Satomi <s-tanaka@bcore.co.jp>
2023-04-07 09:31:21 +0900

    resize image

--- components/blog-content/Image_2.js
+++ components/blog-content/Image_2.js
@@ -6,8 +6,8 @@ export default function Image_2() {
       <Image
         className=""
         src="/images/Blog/2023-0407/hananogosho.png" // Route of the image file
-        height={1836} // Desired size with correct aspect ratio
-        width={4030} // Desired size with correct aspect ratio
+        height={918} // Desired size with correct aspect ratio
+        width={2015} // Desired size with correct aspect ratio
         alt="三月大歌舞伎【花の御所始末】"
       />
       <p className="text-right font-ud lg:text-base text-gray-700 md:text-xs sm:text-sm">
