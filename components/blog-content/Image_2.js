8c2b811e340e5798fb624c64d33e3ba816414481
Satomi <s-tanaka@bcore.co.jp>
2023-04-07 10:09:03 +0900

    fix image loading

--- components/blog-content/Image_2.js
+++ components/blog-content/Image_2.js
@@ -1,15 +1,8 @@
-import Image from "next/image";
+
 
 export default function Image_2() {
   return (
     <div className="">
-      <Image
-        className=""
-        src="/images/Blog/2023-0407/kabuki.png" // Route of the image file
-        height={918} // Desired size with correct aspect ratio
-        width={2015} // Desired size with correct aspect ratio
-        alt="三月大歌舞伎花の御所始末"
-      />
       <p className="text-right font-ud lg:text-base text-gray-700 md:text-xs sm:text-sm">
         写真引用元：松竹株式会社
       </p>
