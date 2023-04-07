e2f6d6a91279432663371b820de322f7821252bf
Satomi <s-tanaka@bcore.co.jp>
2023-04-07 09:52:26 +0900

    remove brackets

--- components/blog-content/Image_2.js
+++ components/blog-content/Image_2.js
@@ -8,7 +8,7 @@ export default function Image_2() {
         src="/images/Blog/2023-0407/kabuki.png" // Route of the image file
         height={918} // Desired size with correct aspect ratio
         width={2015} // Desired size with correct aspect ratio
-        alt="三月大歌舞伎【花の御所始末】"
+        alt="三月大歌舞伎花の御所始末"
       />
       <p className="text-right font-ud lg:text-base text-gray-700 md:text-xs sm:text-sm">
         写真引用元：松竹株式会社
