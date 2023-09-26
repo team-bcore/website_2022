5846301857450d6d68e237e7eee5621b42138fc9
Satomi <s-tanaka@bcore.co.jp>
2023-09-26 12:03:25 +0900

    fix component

--- components/six-dof/Colorbit2.js
+++ components/six-dof/Colorbit2.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Intro() {
+export default function Colorbit2() {
   return (
     <div className="bg-neutral-900 -mb-4">
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
