6aa33c2a97796bae8d88d21a4d95ce3c1a0dab0c
nekondesign <159257512+nekondesign@users.noreply.github.com>
2024-04-06 20:34:40 +0900

    20240406codetuika

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'[);
+})(window,document,'script','dataLayer','GTM-MHQFJVF5');</script>
+<!-- End Google Tag Manager -->
           <link
             href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@700&display=swap"
             rel="stylesheet"
@@ -20,6 +27,10 @@ class MyDocument extends Document {
           ></link>
         </Head>
         <body>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHQFJVF5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
           <Main />
           <NextScript />
         </body>
