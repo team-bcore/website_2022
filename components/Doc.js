258c8e6210512dda3cf0b1199fffa447598b90d2
nekondesign <159257512+nekondesign@users.noreply.github.com>
2025-04-23 10:09:58 +0900

    syusei0422-1

--- components/Doc.js
+++ components/Doc.js
@@ -332,7 +332,6 @@ export default function Doc() {
                   d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
                 />
               </svg>
-            </p>
           </a>
           <a href="https://drive.google.com/uc?export=download&id=14K1MncMOyDa5qEPM7GlokXnBOpkHzUSy">
             <p className="pt-2 font-ud text-base text-gray-700 hover:text-cyan-600 md:text-lg sm:px-4">
